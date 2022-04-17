9e557be0934ab19ea0881e1b25432b005c7af686
Minno <HP@MacBook-Air.local>
2022-04-17 21:13:43 +0300

    Day 1 With Python

---
+++ day_1/helloworld.py
@@ -0,0 +1,43 @@
+# Level 2
+# Example 1
+from cmath import sqrt
+from turtle import distance
+
+
+print(8*16)
+print(6%2)
+
+# Example 2
+print('Mine')
+print('Sunar')
+print('Turkey')
+print('I am enjoying Python')
+
+# Example 3
+print(type(10))                 
+print(type(3.14))               
+print(type(9.8))                
+print(type('4-4j'))          
+print(type(['Asabeneh', 'Python', 'Finland']))           
+print(type({'name':'Mine'}))
+print(type({'surname':'Sunar'})) 
+print(type({'country':'Turkey'}))
+
+# Level 3
+# Example 1
+print(5)
+print(7.5)
+print(1+8j)
+print('Gözde')
+print({'is here':True})
+print(['USA','UK', 'Canada','France'])
+print({
+'first_name':'Mine',
+'country':'Turkey',
+'age':'23'
+})
+print('Mine','Arda','Züleyha','Mehmet')  #Names
+print({90,40,70})
+
+# Example 2
+print(89** (1/2)) 
---
+++ helloworld.py
@@ -0,0 +1,35 @@
+# Day 1 - 30DaysOfPython challange
+print(2+3)               # addition(+)
+print(3-1)               # ubtraction(-)
+print(35*1)              # multiplication(*)
+print(3**3)              # exponential(**)
+print(3 // 2)            # Floor division operator(//)
+
+# checking data types
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex number
+print(type('Asabeneh'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+
+
+# Bunlar benim örneklerim
+print('Mine')
+print('Sunar')
+print('Turkey')
+print('I am enjoying 30 days of python')
+
+print(type(10))                 
+print(type(3.14))               
+print(type(9.8))                
+print(type('4-4j'))          
+print(type(['Asabeneh', 'Python', 'Finland']))           
+print(type({'name':'Mine'}))
+print(type({'surname':'Sunar'})) 
+print(type({'country':'Turkey'}))
+
+
