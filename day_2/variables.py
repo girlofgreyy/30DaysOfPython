a59200d477df83586110d2b566924185c55ba199
Minno <HP@MacBook-Air.local>
2022-04-18 21:00:11 +0300

    day2 finished

---
+++ day_2/variables.py
@@ -0,0 +1,38 @@
+# Day 2: 30 Days of python programming
+# Exercise 1 
+from ast import Compare
+
+
+first_name = 'Mine'
+last_name = 'Sunar'
+full_name = 'Mine Sunar'
+country = 'Türkiye'
+city = 'Osmaniye'
+age = '23'
+year = '2022'
+is_married = False
+is_true = 'I am going home tomorrow'
+is_light_on = 'Yes'
+personal_info = {
+    'firstname' : 'Mine',
+    'lastname' : 'Sunar',
+    'age' : 23,
+
+    }
+
+# Exercise 2
+# Question 1
+print(type('Mine'))
+print(type('Sunar'))
+print(type('23'))
+print(type('False'))
+
+# Question 2
+len('Mine')
+
+# Question 3
+
+# Question 4
+# Question 5
+# Question 6
+# Question 7
