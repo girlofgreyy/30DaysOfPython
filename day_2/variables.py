80ad3d4f79a29fea9c26442615ed41c07fa753f7
Minno <HP@MacBook-Air.local>
2022-04-20 22:38:09 +0300

    day2 finished

--- day_2/variables.py
+++ day_2/variables.py
@@ -31,8 +31,41 @@ print(type('False'))
 len('Mine')
 
 # Question 3
+print(len('Mine') - len('Sunar'))
 
 # Question 4
+num_one = 5 
+num_two = 4
+
+num_one, num_two = '5', '4'
+print('Number One:', num_one)
+print('Number Two:',num_two)
+variable_total = 9
+variable_diff = -1
+variable_product = 20
+print(4/5)
+variable_division = 0.8
+print(4 % 5)
+variable_remainder = 4
+print(5**4)
+variable_exp = 5**4
+print (5 // 4)
+floor_division = 1
+
 # Question 5
+area_of_circle = 188.49
+circum_of_circle = 188.49
+radius = input('30')
+
 # Question 6
+first_name = input('What is your name: ')
+print(first_name)
+last_name = input('What is your last name?')
+print(last_name)
+age = input('How old are you? ')
+print(age)
+country = input('Where are you from?')
+print(country)
+
 # Question 7
+help('keywords')
